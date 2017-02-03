6574f0774cb22d07a63d94b8ed6941a56bb9487f
molly <hulilzster@gmail.com>
2017-02-03 16:04:05 +0800

    step7 uploader resume

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
     resources :jobs do
       member do
         post :publish
-        post :hide 
+        post :hide
       end
     end
   end
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
